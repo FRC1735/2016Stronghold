ff5e1bbb2b28a1453f99135ca68887b783c83905
Scott Taylor <s-taylor@att.net>
2016-01-19 22:02:06 -0500

    Joysticks connected to drivetrain
    Added simple user code to send joysticks to the drivetrain using
    TankDrive.  This should be the base needed to test the drivetrain for
    the robot.

--- src/org/usfirst/frc1735/Stronghold2016/subsystems/DriveTrain.java
+++ src/org/usfirst/frc1735/Stronghold2016/subsystems/DriveTrain.java
@@ -11,8 +11,11 @@
 
 package org.usfirst.frc1735.Stronghold2016.subsystems;
 
+import org.usfirst.frc1735.Stronghold2016.Robot;
 import org.usfirst.frc1735.Stronghold2016.RobotMap;
 import org.usfirst.frc1735.Stronghold2016.commands.*;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.Talon;
@@ -50,5 +53,52 @@ public class DriveTrain extends Subsystem {
         // Set the default command for a subsystem here.
         // setDefaultCommand(new MySpecialCommand());
     }
+    
+    // This is just a wrapper so the joystick and direct methods live in the same class for consistency.
+    public void tankDriveWithJoysticks(Joystick left, Joystick right) {
+        // Collect the Joystick info.
+        // We need to filter out very small joystick values and clamp them to zero
+        // so that a slightly off-center joystick doesn't send signals to the motor.
+        double driveLeft = left.getY();
+        double driveRight = right.getY();
+        
+        //Apply Filter
+        if (Math.abs(driveLeft) < Robot.m_joystickFilter) {driveLeft = 0;}
+        if (Math.abs(driveRight) < Robot.m_joystickFilter) {driveRight = 0;}
+
+        this.tankDrive(-driveLeft, -driveRight);
+    }
+       
+    public void tankDrive(double driveLeft,double driveRight) {
+        // The motors spin faster in one direction than the other.
+        // For this robot configuration:
+        //     The right side motors spin faster in the forward direction.
+        //     The left side motors spin faster in the backward direction.
+        // Create a compensator for this
+        // Can't add to slower drive if it's already 1.0, so must subtract off faster unit.
+        // Actually, we want to use a percentage rather than subtracting a factor.
+        // Algo:
+        // Only compensate if both values are the same (intending to track straight, or are in autonomous)
+        //   if magnitude positive, reduce right side speed.
+        //   if magnitude negative, reduce left  side speed.
+        
+        // We have some funny negative signs floating around, so be sure to use absolute values for this check
+//        if (Math.abs(driveLeft) == Math.abs(driveRight)) {
+//            System.out.println("Compensating for input drive magnitude Left = " + driveLeft + " Right = " + driveRight);
+//            
+//            double motorCompensation = SmartDashboard.getNumber("motorCompensation", .1); // a 10% default should be easy to see if we accidentally got it
+//        
+//            if (driveRight > 0)
+//                driveRight = driveRight * motorCompensation; 
+//            else
+//                driveLeft  = driveLeft  * motorCompensation;
+//            //in the zero case, no compensation needed.
+//        }
+//        // Drive with the compensated value.
+//        System.out.println("After Compensation Left = " + driveLeft + " Right = " + driveRight);
+    	
+        robotDrive21.tankDrive(driveLeft, driveRight); // Optional third arg bool SquaredInputs, when true, decreases sensitivity at low speeds.
+    }
+
 }
 
